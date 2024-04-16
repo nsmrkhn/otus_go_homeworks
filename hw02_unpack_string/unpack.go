ac54a3c2aba9e17b47ed8361ea08768ce87bbfe1
Nikita <36296960+nsmrkhn@users.noreply.github.com>
2024-04-16 09:33:10 +0300

    HW2 is completed (#2)
    * HW2 is completed
    
    * fixes after review
    
    ---------
    
    Co-authored-by: Nikita Semerukhin <nikita.semerukhin>

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,40 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	runesArray := []rune(s)
+	var resultString string
+
+	for i, value := range runesArray {
+		if i == 0 && unicode.IsDigit(value) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(value) && unicode.IsDigit(runesArray[i-1]) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(value) {
+			multiplier, err := strconv.Atoi(string(value))
+			if err != nil {
+				return "", err
+			}
+
+			resultString = resultString[:len(resultString)-1]
+			if multiplier != 0 {
+				for j := 0; j < multiplier; j++ {
+					resultString += string(runesArray[i-1])
+				}
+			}
+		} else {
+			resultString += string(value)
+		}
+	}
+	return resultString, nil
 }
